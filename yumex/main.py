6adc49126a9ef6990439b5e37ae1fc1839b361c4
Tim Lauridsen <tim.lauridsen@gmail.com>
2022-11-25 17:21:05 +0100

    Add dummy action handlers

--- yumex/main.py
+++ yumex/main.py
@@ -1,4 +1,3 @@
-
 import sys
 
 from gi.repository import Gtk, Gdk, Gio, Adw, GLib
@@ -10,6 +9,7 @@ from yumex.constants import (
     rel_ver,
     version,
 )
+
 
 class YumexApplication(Adw.Application):
     """The main application singleton class."""
@@ -32,10 +32,39 @@ class YumexApplication(Adw.Application):
             self.win = YumexMainWindow(
                 application=self,
             )
+
+        self.create_action("apply_actions", self.on_apply_actions)
+        self.create_action("about", self.on_about)
+        self.create_action("preferences", self.on_preferences)
+
         self.win.present()
+
+    def create_action(self, name, callback, shortcuts=None):
+        """Add an application action.
+
+        Args:
+            name: the name of the action
+            callback: the function to be called when the action is
+            activated
+            shortcuts: an optional list of accelerators
+        """
+        action = Gio.SimpleAction.new(name, None)
+        action.connect("activate", callback)
+        self.add_action(action)
+        if shortcuts:
+            self.set_accels_for_action(f"app.{name}", shortcuts)
+
+    def on_apply_actions(self, widget, data):
+        self.win.show_message("Apply pressed")
+
+    def on_about(self, widget, data):
+        self.win.show_message("About pressed")
+
+    def on_preferences(self, widget, data):
+        self.win.show_message("Preferences pressed")
 
 def main():
     """The application's entry point."""
-    print('yumex is running')
+    print("yumex is running")
     app = YumexApplication()
     return app.run(sys.argv)
