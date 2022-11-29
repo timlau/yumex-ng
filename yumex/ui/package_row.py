70e8844f005671a3b8b929fc4f07781ace3816fd
Tim Lauridsen <tim.lauridsen@gmail.com>
2022-11-29 19:53:50 +0100

    change package_row look

--- yumex/ui/package_row.py
+++ yumex/ui/package_row.py
@@ -11,8 +11,6 @@ class YumexPackageRow(Adw.ActionRow):
     installed = Gtk.Template.Child("installed")
     version = Gtk.Template.Child("version")
     repo = Gtk.Template.Child("repo")
-    install_button = Gtk.Template.Child("install-button")
-    remove_button = Gtk.Template.Child("remove-button")
 
     def __init__(self, name, desc, repo="fedora",version="1.0.0-23", installed=False, **kwargs):
         super().__init__(**kwargs)
@@ -22,9 +20,9 @@ class YumexPackageRow(Adw.ActionRow):
         self.set_subtitle(desc)
         self.installed.set_visible(installed)
         if installed:
-            self.install_button.set_visible(False)
+            # self.set_icon_name("user-trash-symbolic")
             self.add_css_class("accent")
-        else:
-            self.remove_button.set_visible(False)
+        # else:
+            # self.set_icon_name("system-software-install-symbolic")
         self.version.set_label(version)
         self.repo.set_label(repo)            
