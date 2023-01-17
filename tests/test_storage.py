e66f2aaeedcdaa4dfdb9b472e007921d28f285b6
Tim Lauridsen <tim.lauridsen@gmail.com>
2023-01-17 11:10:22 +0100

    test: add test for insert_sorted with illegal type

--- tests/test_storage.py
+++ tests/test_storage.py
@@ -10,30 +10,30 @@ def storage() -> PackageStorage:
     return PackageStorage()
 
 
-def test_storage_setup(storage):
+def test_storage_setup(storage: PackageStorage):
     assert isinstance(storage.get_storage(), Gio.ListStore)
 
 
-def test_storage_add_pkg(storage, pkg):
+def test_storage_add_pkg(storage: PackageStorage, pkg):
     storage.add_package(pkg)
     assert len(storage) == 1
     assert storage.get_storage()[0] == pkg
 
 
-def test_storage_add_pkgs(storage, pkg, pkg_other):
+def test_storage_add_pkgs(storage: PackageStorage, pkg, pkg_other):
     storage.add_packages([pkg, pkg_other])
     assert len(storage) == 2
     assert storage.get_storage()[0] == pkg
     assert storage.get_storage()[1] == pkg_other
 
 
-def test_storage_contains(storage, pkg, pkg_other):
+def test_storage_contains(storage: PackageStorage, pkg, pkg_other):
     storage.add_packages([pkg])
     assert pkg in storage
     assert pkg_other not in storage
 
 
-def test_storage_iterate(storage, pkg, pkg_other):
+def test_storage_iterate(storage: PackageStorage, pkg, pkg_other):
     storage.add_packages([pkg, pkg_other])
     assert len(storage) == 2
     for ndx, po in enumerate(storage):
@@ -46,7 +46,7 @@ def test_storage_iterate(storage, pkg, pkg_other):
                 raise IndexError("There should not be more than 2 packages in store")
 
 
-def test_storage_clear(storage, pkg, pkg_other):
+def test_storage_clear(storage: PackageStorage, pkg, pkg_other):
     """test clear()"""
 
     storage.add_packages([pkg, pkg_other])
@@ -57,7 +57,7 @@ def test_storage_clear(storage, pkg, pkg_other):
     assert len(storage) == 0
 
 
-def test_storage_insert_sorted(storage, pkg, pkg_other):
+def test_storage_insert_sorted(storage: PackageStorage, pkg, pkg_other):
     """test sort by name"""
     storage.add_package(pkg_other)
     storage.insert_sorted(pkg, lambda a, b: a.name > b.name)
@@ -65,7 +65,14 @@ def test_storage_insert_sorted(storage, pkg, pkg_other):
     assert storage.get_storage()[1] == pkg_other
 
 
-def test_storage_sort_name(storage, pkg, pkg_other):
+def test_storage_insert_sorted_illegal(storage: PackageStorage, pkg, pkg_other):
+    """test sort by name"""
+    storage.add_package(pkg_other)
+    with pytest.raises(ValueError):
+        storage.insert_sorted("illegal", lambda a, b: a.name > b.name)
+
+
+def test_storage_sort_name(storage: PackageStorage, pkg, pkg_other):
     """test sort by name"""
     storage.add_packages([pkg_other, pkg])  # otherpkg, mypkg
     storage.sort_by(SortType.NAME)  # mypkg, otherpkg
@@ -73,7 +80,7 @@ def test_storage_sort_name(storage, pkg, pkg_other):
     assert storage.get_storage()[1] == pkg_other
 
 
-def test_storage_sort_repo(storage, pkg, pkg_other):
+def test_storage_sort_repo(storage: PackageStorage, pkg, pkg_other):
     """test sort by repo"""
     storage.add_packages([pkg, pkg_other])  # repo2, repo1
     storage.sort_by(SortType.REPO)  # repo1, repo2
@@ -81,7 +88,7 @@ def test_storage_sort_repo(storage, pkg, pkg_other):
     assert storage.get_storage()[1] == pkg
 
 
-def test_storage_sort_size(storage, pkg, pkg_other):
+def test_storage_sort_size(storage: PackageStorage, pkg, pkg_other):
     """test sort by size"""
     storage.add_packages([pkg, pkg_other])  # 2048, 1024
     storage.sort_by(SortType.SIZE)  # 1024, 2048
@@ -89,7 +96,7 @@ def test_storage_sort_size(storage, pkg, pkg_other):
     assert storage.get_storage()[1] == pkg
 
 
-def test_storage_sort_arch(storage, pkg, pkg_other):
+def test_storage_sort_arch(storage: PackageStorage, pkg, pkg_other):
     """test sort by size"""
     storage.add_packages([pkg, pkg_other])  # x86_64, noarch
     storage.sort_by(SortType.ARCH)  # noarch, x86_64
@@ -97,14 +104,14 @@ def test_storage_sort_arch(storage, pkg, pkg_other):
     assert storage.get_storage()[1] == pkg
 
 
-def test_storage_sort_illegal(storage, pkg, pkg_other):
+def test_storage_sort_illegal(storage: PackageStorage, pkg, pkg_other):
     """test sort by illegal type"""
     with pytest.raises(ValueError):
         storage.add_packages([pkg, pkg_other])  # x86_64, noarch
         storage.sort_by("notfound")  # noarch, x86_64
 
 
-def test_storage_add_illegal(storage, pkg, pkg_other):
+def test_storage_add_illegal(storage: PackageStorage, pkg, pkg_other):
     """test adding illegal datatypes"""
     with pytest.raises(ValueError):
         storage.add_packages([pkg, pkg_other, "illegal"])
@@ -112,7 +119,7 @@ def test_storage_add_illegal(storage, pkg, pkg_other):
         storage.add_packages([pkg, pkg_other, None])
 
 
-def test_storage_find_by_nevra(storage, pkg, pkg_other, pkg_upd):
+def test_storage_find_by_nevra(storage: PackageStorage, pkg, pkg_other, pkg_upd):
     storage.add_packages([pkg, pkg_other])  # x86_64, noarch
     po = storage.find_by_nevra(pkg.nevra)  # pkg is in storage
     assert po == pkg
