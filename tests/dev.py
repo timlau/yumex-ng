94daf166bcd7001b3a130414cd385a65c594ee7e
Tim Lauridsen <tim.lauridsen@gmail.com>
2023-02-01 19:28:09 +0100

    test: test is the setup of a package_view test is possible.
    Not very useful for unit testing, but can be useful for tracking down a bug

--- tests/dev.py
+++ tests/dev.py
@@ -1,30 +1,29 @@
+from unittest.mock import Mock
 import pytest
 import gi
+
 
 gi.require_version("Gtk", "4.0")
 gi.require_version("Adw", "1")
 
 from gi.repository import Gtk
-from .mock import TemplateUIFromFile
+from .mock import TemplateUIFromFile, mock_presenter
 
 
 @pytest.fixture
-def progress(mocker, monkeypatch):
+def view(monkeypatch):
     """create a progress object"""
     # used the Special Gtk.Template wrapper
     monkeypatch.setattr(Gtk, "Template", TemplateUIFromFile)
 
-    from yumex.ui.progress import YumexProgress
+    from yumex.ui.queue_view import YumexQueueView
+    from yumex.ui.pachage_view import YumexPackageView
 
-    progress = YumexProgress()
-    return progress
-
-
-def test_set_title(progress):
-    progress.set_title("titel")
-    assert progress.title.get_label() == "titel"
+    presenter = mock_presenter()
+    queue_view = YumexQueueView(presenter=presenter)
+    view = YumexPackageView(win=Mock(), presenter=presenter, qview=queue_view)
+    return view
 
 
-def test_set_subtitle(progress):
-    progress.set_subtitle("subtitel")
-    assert progress.subtitle.get_label() == "subtitel"
+def test_setup(view):
+    assert True
