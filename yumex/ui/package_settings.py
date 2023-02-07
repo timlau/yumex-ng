a3ca4b78353c05d92ae639f58cf2f4036f632bb0
Tim Lauridsen <tim.lauridsen@gmail.com>
2023-02-07 07:31:12 +0100

    refactor:  add docstrings
    - move icons setting to .blp

--- yumex/ui/package_settings.py
+++ yumex/ui/package_settings.py
@@ -13,7 +13,7 @@
 #
 # Copyright (C) 2023  Tim Lauridsen
 
-from gi.repository import Gtk, GObject
+from gi.repository import Gtk, GObject, Adw
 
 from yumex.constants import ROOTDIR
 from yumex.utils import log
@@ -24,27 +24,25 @@ from yumex.utils.enums import PackageFilter, SortType, InfoType
 class YumexPackageSettings(Gtk.Box):
     __gtype_name__ = "YumexPackageSettings"
 
-    filter_available = Gtk.Template.Child()
-    filter_installed = Gtk.Template.Child()
-    filter_updates = Gtk.Template.Child()
-    filter_search = Gtk.Template.Child()
-    sort_by = Gtk.Template.Child()
-    info_type = Gtk.Template.Child()
-    show_icon = Gtk.Template.Child()
-    sort_icon = Gtk.Template.Child()
-    installed_row = Gtk.Template.Child()
+    filter_available: Gtk.CheckButton = Gtk.Template.Child()
+    filter_installed: Gtk.CheckButton = Gtk.Template.Child()
+    filter_updates: Gtk.CheckButton = Gtk.Template.Child()
+    filter_search: Gtk.CheckButton = Gtk.Template.Child()
+    sort_by: Adw.ComboRow = Gtk.Template.Child()
+    info_type: Adw.ComboRow = Gtk.Template.Child()
+    installed_row: Adw.ActionRow = Gtk.Template.Child()
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
         self.current_pkg_filter: PackageFilter = None
         self.previuos_pkg_filter: PackageFilter = None
-        self.show_icon.set_from_icon_name("diagnostics-symbolic")
-        self.sort_icon.set_from_icon_name("view-sort-descending-rtl-symbolic")
 
     def set_focus(self):
+        """Set focus on the settings sidebar."""
         self.installed_row.grab_focus()
 
     def set_active_filter(self, pkg_filter: PackageFilter):
+        """Set the active package filter"""
         match pkg_filter:
             case PackageFilter.UPDATES:
                 self.filter_updates.activate()
@@ -54,6 +52,7 @@ class YumexPackageSettings(Gtk.Box):
                 self.filter_available.activate()
 
     def unselect_all(self):
+        """Unselect all filters, so we can re-select one"""
         self.previuos_pkg_filter = None
         self.filter_available.set_active(False)
         self.filter_installed.set_active(False)
@@ -61,14 +60,17 @@ class YumexPackageSettings(Gtk.Box):
         self.filter_search.set_active(True)
 
     def get_info_type(self) -> InfoType:
+        """get the current info type"""
         selected = self.info_type.get_selected()
         return list(InfoType)[selected]
 
     def get_sort_attr(self) -> SortType:
+        """get the current sort attribute"""
         selected = self.sort_by.get_selected()
         return list(SortType)[selected]
 
     def on_package_filter_activated(self, button):
+        """handler for package filter changes"""
         pkg_filter: PackageFilter = PackageFilter(button.get_name())
         self.current_pkg_filter = pkg_filter
         if self.current_pkg_filter != self.previuos_pkg_filter:
@@ -93,12 +95,15 @@ class YumexPackageSettings(Gtk.Box):
 
     @GObject.Signal(arg_types=(str,))
     def package_filter_changed(self, pkg_filter: PackageFilter):
+        """signal emitted when a package filter is changed"""
         pass
 
     @GObject.Signal(arg_types=(str,))
     def sort_attr_changed(self, sort_attr: SortType):
+        """signal emitted when a sort attribute is changed"""
         pass
 
     @GObject.Signal(arg_types=(str,))
     def info_type_changed(self, info_type: InfoType):
+        """signal emitted when a info type is changed"""
         pass
