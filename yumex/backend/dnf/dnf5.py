ff045f56a26c946f145e4f61ef36592064d6be09
Tim Lauridsen <tim.lauridsen@gmail.com>
2023-05-31 09:07:39 +0200

    dnf5: remove debug printing

--- yumex/backend/dnf/dnf5.py
+++ yumex/backend/dnf/dnf5.py
@@ -237,11 +237,6 @@ class Backend(dnf.Base):
         log(f" DNF5: depsolve completted : {problems}")
         if problems == dnf.GoalProblem_NO_PROBLEM:
             for tspkg in transaction.get_transaction_packages():
-                print(
-                    tspkg.get_package().get_nevra(),
-                    tspkg.get_action(),
-                    dnf.Transaction.Transaction,
-                )
                 action = tspkg.get_action()
                 pkg = create_package(tspkg.get_package())
                 if pkg.nevra not in nevra_dict:
