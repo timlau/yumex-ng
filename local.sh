015dde71fc140fb777a7b32b12d5a8cafb2fef3f
Tim Lauridsen <tla@rasmil.dk>
2024-06-14 11:59:39 +0200

    build: make the updater systemd config relocatable
    So local bulds work without sudo access
    - install the yumex_updater_systray in the bin dir (no symlink)
    - remove meson -with_updater option (not needed any more)

--- local.sh
+++ local.sh
@@ -10,9 +10,9 @@ echo "Rebuilding"
 meson setup builddir
 if [ -z "$1" ]
   then
-    meson configure builddir -Dprefix="$(pwd)/builddir" -Dbuildtype=debug -Dwith_updater=false
+    meson configure builddir -Dprefix="$(pwd)/builddir" -Dbuildtype=debug 
   else
-    meson configure builddir -Dprefix="$(pwd)/builddir" -Dbuildtype=debug -Dwith_updater=false -Ddnf_backend=$1
+    meson configure builddir -Dprefix="$(pwd)/builddir" -Dbuildtype=debug -Ddnf_backend=$1
 fi
 ninja -C builddir install
 
