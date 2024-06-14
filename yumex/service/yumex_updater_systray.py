ad377089286a257ab05815e77f49d94036fb247b
Tim Lauridsen <tim.lauridsen@gmail.com>
2024-06-14 07:20:46 +0200

    updater: remove unused imports

--- yumex/service/yumex_updater_systray.py
+++ yumex/service/yumex_updater_systray.py
@@ -1,16 +1,13 @@
 #!/usr/bin/python3
-
 import gi
-gi.require_version('Gtk', '3.0')
-gi.require_version('AppIndicator3', '0.1')
-gi.require_version('GLib', '2.0')
-gi.require_version('Flatpak', '1.0')
+
+gi.require_version("Gtk", "3.0")
+gi.require_version("AppIndicator3", "0.1")
+gi.require_version("GLib", "2.0")
+gi.require_version("Flatpak", "1.0")
 from gi.repository import Gtk, AppIndicator3, GLib, Flatpak
 import subprocess
 from pathlib import Path
-import cairosvg
-import io
-import os
 import shutil
 import threading
 import time
@@ -20,6 +17,7 @@ import dbus.mainloop.glib
 import configparser
 
 from yumex.constants import BACKEND
+
 if BACKEND == "DNF5":
     from yumex.service.dnf5 import UpdateChecker
 else:
@@ -30,9 +28,9 @@ from packaging import version
 
 # Define paths
 home_dir = Path.home()
-config_dir = home_dir / '.config' / 'yumex'
-default_config_path = '/usr/share/yumex/yumex-service.conf'
-user_config_path = config_dir / 'yumex-service.conf'
+config_dir = home_dir / ".config" / "yumex"
+default_config_path = "/usr/share/yumex/yumex-service.conf"
+user_config_path = config_dir / "yumex-service.conf"
 
 # Create the config directory if it doesn't exist
 config_dir.mkdir(parents=True, exist_ok=True)
@@ -45,28 +43,30 @@ if not user_config_path.exists():
 config = configparser.ConfigParser()
 config.read(user_config_path)
 
-custom_updater = config.get('DEFAULT', 'custom_updater', fallback=None)
-always_hide = config.getboolean('DEFAULT', 'always_hide', fallback=False)
-update_sync_interval = config.getint('DEFAULT', 'update_sync_interval', fallback=3600)
+custom_updater = config.get("DEFAULT", "custom_updater", fallback=None)
+always_hide = config.getboolean("DEFAULT", "always_hide", fallback=False)
+update_sync_interval = config.getint("DEFAULT", "update_sync_interval", fallback=3600)
+
 
 class UpdateService(dbus.service.Object):
     def __init__(self, bus_name: dbus.service.BusName, object_path: str) -> None:
         super().__init__(bus_name, object_path)
 
-    @dbus.service.method('com.yumex.UpdateService')
+    @dbus.service.method("com.yumex.UpdateService")
     def RefreshUpdates(self) -> None:
         refresh_updates()
+
 
 def on_clicked_update(widget: Gtk.Widget) -> None:
     if custom_updater:
         subprocess.Popen([custom_updater])
+
 
 def on_clicked_pm(widget: Gtk.Widget) -> None:
     subprocess.Popen(["/usr/bin/yumex"])
 
 
 def refresh_updates(widget: Gtk.Widget = None) -> None:
-
     sys_update_count = len(UpdateChecker())
 
     flatpak_user_count = len(Flatpak.Installation.new_user().list_installed_refs_for_update())
@@ -90,15 +90,17 @@ def refresh_updates(widget: Gtk.Widget = None) -> None:
         else:
             GLib.idle_add(indicator.set_status, AppIndicator3.IndicatorStatus.PASSIVE)
 
+
 def check_updates() -> None:
     while True:
         refresh_updates()
         time.sleep(update_sync_interval)
 
+
 indicator = AppIndicator3.Indicator.new(
-    'System Update Monitor',
+    "System Update Monitor",
     "/usr/share/icons/hicolor/scalable/apps/yumex-system-software-update.svg",
-    AppIndicator3.IndicatorCategory.APPLICATION_STATUS
+    AppIndicator3.IndicatorCategory.APPLICATION_STATUS,
 )
 indicator.set_status(AppIndicator3.IndicatorStatus.ACTIVE)
 indicator.set_title("Checking for updates...")
@@ -124,8 +126,8 @@ indicator.set_menu(menu)
 
 dbus.mainloop.glib.DBusGMainLoop(set_as_default=True)
 session_bus = dbus.SessionBus()
-bus_name = dbus.service.BusName('com.yumex.UpdateService', session_bus)
-update_service = UpdateService(bus_name, '/com/yumex/UpdateService')
+bus_name = dbus.service.BusName("com.yumex.UpdateService", session_bus)
+update_service = UpdateService(bus_name, "/com/yumex/UpdateService")
 
 refresh_updates()
 
