be8000a5230bfecba9871a09a42b3c2c50b5190e
Tim Lauridsen <tla@rasmil.dk>
2025-04-20 19:16:30 +0200

    fix param

--- yumex/backend/dnf5daemon/client.py
+++ yumex/backend/dnf5daemon/client.py
@@ -352,5 +352,6 @@ class Dnf5DbusClient:
         logger.debug(f"DBUS: {self.session_offline.object_path}.set_finish_action()")
         reboot = self._async_method("set_finish_action", proxy=self.session_offline)
         res, err = list(reboot("reboot"))
+        res = list(res)
         logger.debug(f"offline_reboot() returned : {res, err}")
         return res, err
