8561508f0b73e0c67cc089c10613d584713ccdab
Tim Lauridsen <tla@rasmil.dk>
2025-04-21 09:36:45 +0200

    adjust how problems show text

--- tests/guitest/test.py
+++ tests/guitest/test.py
@@ -1,8 +1,8 @@
 def run_test(win):
     # test_transaction_result_show_errors(win)
-    # test_transaction_result_show_errors_long(win)
+    test_transaction_result_show_errors_long(win)
     # test_search_settings(win)
-    test_system_upgrade_dialog(win)
+    # test_system_upgrade_dialog(win)
     return
 
 
@@ -22,7 +22,7 @@ def test_transaction_result_show_errors(win):
     """Test the transaction result dialog."""
     result = YumexTransactionResult()
     msg = "Something is rotten in the state of Denmark\n" * 10
-    msg += "10 < 11\n"  # This need to be escaped else it will be interpreted as a tag
+    # msg += "10 < 11\n"  # This need to be escaped else it will be interpreted as a tag
     result.show_errors(msg)
     result.present(win)
 
